aa5a2c3ae59678b43cc8774abd7f6af03f061f92
Nicholas Humfrey <njh@aelius.com>
2018-10-03 19:34:50 +0100

    Fixed bug in the bearer caching

--- lib/bearer_resolver.rb
+++ lib/bearer_resolver.rb
@@ -24,7 +24,7 @@ def resolve_bearer_id(params)
     if File.exist?(CACHE_FILENAME)
       begin
         data = JSON.parse(File.read(CACHE_FILENAME))
-        BEARER_CACHE.merge(data)
+        BEARER_CACHE.merge!(data)
       rescue => e
         $stderr.puts "Failed to load bearer cache from disk: #{e}"
       end
@@ -33,7 +33,6 @@ def resolve_bearer_id(params)
 
   unless BEARER_CACHE.has_key?(fqdn)
     begin
-
       result = RadioDNS::Resolver.resolve(fqdn)
       if result.nil?
         BEARER_CACHE[fqdn] = nil
@@ -44,11 +43,11 @@ def resolve_bearer_id(params)
     rescue Resolv::ResolvError
       BEARER_CACHE[fqdn] = nil
     end
-  end
 
-  # Write cache to disk
-  File.open(CACHE_FILENAME, 'wb') do |file|
-    file.write JSON.pretty_generate(BEARER_CACHE)
+    # Write cache to disk
+    File.open(CACHE_FILENAME, 'wb') do |file|
+      file.write JSON.pretty_generate(BEARER_CACHE)
+    end
   end
 
   return BEARER_CACHE[fqdn]
