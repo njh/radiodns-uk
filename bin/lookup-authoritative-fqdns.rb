4584fc88ce1ccf7e50116ead0ef0c6bf9895496f
Nicholas Humfrey <njh@aelius.com>
2018-10-19 00:55:06 +0100

    Create a 'NULL' authority before any of the real ones

--- bin/lookup-authoritative-fqdns.rb
+++ bin/lookup-authoritative-fqdns.rb
@@ -2,6 +2,9 @@
 
 require_relative '../models'
 
+# Make sure the 'null' database entry exists
+Authority.find_or_create(:fqdn => nil)
+
 Bearer.all.each do |bearer|
   puts "#{bearer.fqdn} (#{bearer[:ofcom_label]})"
 
