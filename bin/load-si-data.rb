e0243ee27e68980bfe39e73e5bee2cd979e072a2
Nicholas Humfrey <njh@aelius.com>
2018-10-18 09:35:33 +0100

    Implemented listing and display of services

--- bin/load-si-data.rb
+++ bin/load-si-data.rb
@@ -36,6 +36,86 @@ def parse_logos(element)
   return logos
 end
 
+def validate_bearers(authority, xml)
+  bearers = {}
+  xml.xpath("bearer").each do |xmlbearer|
+    if xmlbearer['id']
+      bearer_id = xmlbearer['id'].downcase
+
+      # Ignore bearers that aren't UK DAB/FM for now
+      next unless bearer_id =~ /^(fm|dab):ce1\./
+
+      # Check for duplicates
+      if bearers.has_key?(bearer_id)
+        $stderr.puts "  => Warning: service has duplicate beaerer IDs"
+        next
+      end
+      
+      params = Bearer.parse_uri(bearer_id)
+      if params.nil?
+        $stderr.puts "  => Warning: invalid bearer: #{bearer_id}"
+        next
+      end
+
+      fqdn = params.resolve_fqdn
+      if fqdn.nil?
+        $stderr.puts "  => Unable to resolve bearer #{bearer_id}"
+        next
+      elsif fqdn != authority.fqdn
+        $stderr.puts "  => FQDN does not match for bearer #{bearer_id} = #{authority.fqdn}"
+        next
+      end
+
+      bearer = Bearer.find_or_create(params.to_hash)
+      bearer.authority = authority
+      bearer.bitrate = xmlbearer['bitrate'].to_i unless xmlbearer['bitrate'].nil?
+      bearer.cost = xmlbearer['cost'].to_i unless xmlbearer['bitrate'].nil?
+      bearer.offset = xmlbearer['offset'].to_i unless xmlbearer['offset'].nil?
+      bearer.mime_type = xmlbearer['mimeValue'] unless xmlbearer['mimeValue'].nil?
+      bearer.save
+
+      # Keep in a hash to help checking for duplicates
+      bearers[bearer_id] = bearer
+    end
+  end
+
+  bearers.values
+end
+
+
+def process_service(authority, xml)
+  service = Service.new(
+    :short_name => xml.content_at('./shortName'),
+    :medium_name => xml.content_at('./mediumName'),
+    :long_name => xml.content_at('./longName'),
+    :short_description => xml.content_at('./mediaDescription/shortDescription'),
+    :long_description => xml.content_at('./mediaDescription/longDescription')
+  )
+  
+  if service.name.nil?
+    $stderr.puts "Service has no name"
+    return
+  else
+    $stderr.puts "  #{service.name}"
+  end
+
+  bearers = validate_bearers(authority, xml)
+  if bearers.empty?
+    $stderr.puts "  => Warning: service has no valid UK DAB or FM bearers"
+    return
+  end
+  
+  # Create the service
+  service.save
+
+  # Add the service ID to each of the bearers
+  bearers.each { |b| b.update(:service_id => service.id) }
+  
+  # FIXME: do this better
+  default_bearer = bearers.sort_by { |b| b.uri }.first
+  service.update(:default_bearer_id => default_bearer.id)
+end
+
 
 def process_service_provider(authority, xml)
   authority.name = xml.content_at('./longName') ||
@@ -54,12 +134,13 @@ end
 
 
 Authority.valid.each do |authority|
+  puts "Loading: #{authority}"
   filepath = authority.si_filepath
   unless File.exist?(filepath)
-    $stderr.puts "File does not exist: #{filepath}"
+    puts "File does not exist: #{filepath}"
     next
-  end
-
+  end 
+  
   begin
     doc = File.open(filepath, 'rb') { |f| Nokogiri::XML(f) }
     doc.remove_namespaces!
@@ -68,12 +149,24 @@ Authority.valid.each do |authority|
     if si
       sp = si.at("./services/serviceProvider")
       process_service_provider(authority, sp) unless sp.nil?
+
+      si.xpath("./services/service").each do |service|
+        begin
+          process_service(authority, service)
+        rescue => e
+          $stderr.puts "Failed to process service: #{filepath}:line (#{e})"
+          $stderr.puts e.backtrace.join("\n")
+        end
+      end
+    else
+      raise "SI file for #{authority.fqdn} does not contain serviceInformation element"
     end
 
   rescue => e
     $stderr.puts "Failed to parse file: #{filepath} (#{e})"
     $stderr.puts e.backtrace.join("\n")
-    authority.set(:have_radioepg => false)
+    authority.update(:have_radioepg => false)
   end
 
+  puts
 end
