e697612f90959136dcd22cf5e65d56bd7b0336dd
Nicholas Humfrey <njh@aelius.com>
2018-10-19 13:32:44 +0100

    Added service logos

---
+++ models/logo.rb
@@ -0,0 +1,25 @@
+class Logo < Sequel::Model
+  many_to_one :service
+  
+  def width
+    size.split('x')[0].to_i
+  end
+
+  def height
+    size.split('x')[1].to_i
+  end
+  
+  def pixels
+    width * height
+  end
+end
+
+# Table: logos
+# Columns:
+#  id         | integer      | PRIMARY KEY AUTOINCREMENT
+#  service_id | integer      |
+#  size       | varchar(9)   |
+#  url        | varchar(255) |
+# Indexes:
+#  logos_service_id_index      | (service_id)
+#  logos_service_id_size_index | UNIQUE (service_id, size)
