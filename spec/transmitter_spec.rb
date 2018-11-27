b23feac1207867cae983f1152a542c8179d4d1ff
Nicholas Humfrey <njh@aelius.com>
2018-11-27 18:36:24 +0000

    Added some tests for the Transmitter model

---
+++ spec/transmitter_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Transmitter do
+  let (:transmitter) {
+    Transmitter.new(
+      :ngr => 'SE222281',
+      :name => 'Emley Moor',
+      :area => 'Yorkshire',
+      :lat => 53.611359,
+      :long => -1.665774,
+      :site_height => 256
+    )
+  }
+
+  describe '#name' do
+    it 'returns the name of the transmitter' do
+      expect(transmitter.name).to eq("Emley Moor")
+    end
+  end
+
+  describe '#path' do
+    it 'returns website path, including the NGR' do
+      expect(transmitter.path).to eq("/transmitters/se222281")
+    end
+  end
+end
