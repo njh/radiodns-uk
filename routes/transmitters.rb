ecd94f09c8cb6470616fba8f6a996bba9bee6be0
Nicholas Humfrey <njh@aelius.com>
2019-02-12 18:02:00 +0000

    Added list of transmitters for each County

--- routes/transmitters.rb
+++ routes/transmitters.rb
@@ -1,14 +1,36 @@
 class App
   route('transmitters') do |r|
     r.get true do
+      @countries = Country.order(:name).all
+
+      r.html { view('transmitters_index') }
+      "Unsupported format"
+    end
+
+    def transmitter_list(r)
+      @alternatives = ['geojson', 'json', 'csv']
+      r.html { view('transmitters_list') }
+      r.geojson { render('transmitters_list.geojson', :engine => 'yajl') }
+      r.json { render('transmitters_list', :engine => 'yajl') }
+      r.csv { render('transmitters_list', :engine => 'rcsv') }
+      "Unsupported format"
+    end
+
+    r.get 'counties', String do |county|
+      @county = County.first!(:url_key => county)
+      @transmitters = Transmitter.where(:county_id => @county.id).
+                      order(:name).
+                      eager(:county)
+
+      @page_title = "List of Radio Transmitters in #{@county.name}"
+      transmitter_list(r)
+    end
+
+    r.get 'all' do
       @transmitters = Transmitter.order(:name).eager(:county).all
 
-      @alternatives = ['geojson', 'json', 'csv']
-      r.html { view('transmitters_index') }
-      r.geojson { render('transmitters_index.geojson', :engine => 'yajl') }
-      r.json { render('transmitters_index', :engine => 'yajl') }
-      r.csv { render('transmitters_index', :engine => 'rcsv') }
-      "Unsupported format"
+      @page_title = "List of Radio Transmitters in the UK"
+      transmitter_list(r)
     end
 
     r.get String do |ngr|
